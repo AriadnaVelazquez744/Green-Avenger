0dbf9444623d9fed7adaf360ff1f2449e6f27fb1
AriadnaVelazquez744 <ariadnavelazquezrey@gmail.com>
2023-10-13 21:50:18 -0400

    AST

---
+++ AST/ASTNode.cs
@@ -0,0 +1,11 @@
+public abstract class ASTNode
+{
+    public CodeLocation Location {get; set;}
+
+    public ASTNode(CodeLocation location)
+    {
+        Location = location;
+    }
+    
+    public abstract bool CheckSemantic(Context context, Scope scope, List<CompilingError> errors);    
+}
