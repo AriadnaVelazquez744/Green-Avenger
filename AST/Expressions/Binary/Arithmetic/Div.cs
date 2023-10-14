bdfec29e51256cc20a94660d025d06e2c4f98a5a
AriadnaVelazquez744 <ariadnavelazquezrey@gmail.com>
2023-10-13 21:55:59 -0400

    binary

---
+++ AST/Expressions/Binary/Arithmetic/Div.cs
@@ -0,0 +1,23 @@
+public class Div :BinaryExpression
+{
+    public Div(CodeLocation location) : base(location) { }
+
+    public override ExpressionType Type { get; set; }
+    public override object? Value { get; set; }
+
+    public override void Evaluate()
+    {
+        Left!.Evaluate();
+        Right!.Evaluate();
+
+        Value = (double)Left.Value! / (double)Right.Value!;
+    }
+
+    public override string? ToString()
+    {
+        if (Value == null)
+            return String.Format("({0} / {1})", Left, Right);
+
+        return Value!.ToString();
+    }
+}
