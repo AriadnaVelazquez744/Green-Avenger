2d36ff422548d0c1428b5e3586023cef3c643810
AriadnaVelazquez744 <ariadnavelazquezrey@gmail.com>
2023-12-12 08:28:01 -0500

    multillamado

--- Program.cs
+++ Program.cs
@@ -3,40 +3,35 @@
 //Se lee la línea introducida en la consola que se va a analizar.
 
 LexicalAnalyzer lex = Lexer.LexicalAnalyzer;
-
-Console.WriteLine("Introduce the code: ");
-string text = Console.ReadLine()!;
-
-//Se inicializa el IEnumerable de los tokens llamando el método correspondiente del lex con el texto
-//Se inicializan también las clases TokenStream, Parser para poder utilizar sus métodos y propiedades, la lista 
-//de errores para ir guardándolos y el nodo de programa elemental que es el que evaluará todas las estructuras que se creen
-Console.WriteLine("Iniciando el análisis");
-
-IEnumerable<Token> tokens = lex.GetTokens(text, new List<CompilingError>());
-
-TokenStream stream = new(tokens);
-Parser parser = new(stream);
-List<CompilingError> errors = new();
-
-MainProgram program = parser.Parse(errors);
+Context context = new();
+Scope scope = new();
 
 
-//Si han ocurrido errores durante el proceso de análisis sintáctico se imprimen en consola, si no se pasa a realizar 
-//el análisis semántico, se repite el proceso de que si han habido errores se imprimen y si no se evalúa y devuelven 
-//los valores correspondientes.
-
-if (errors.Count > 0)
+while (true)
 {
-    foreach (var error in errors)
-        Console.WriteLine("{0}, {1}, {2}", error.Location.Column, error.Code, error.Argument);
-}
-else
-{
-    Context context = new();
-    Scope scope = new();
-
-    program.CheckSemantic(context, scope, errors);
-
+    Console.WriteLine("Introduce the code: ");
+    string text = Console.ReadLine()!;
+    if (text == "")
+        break;
+    
+    //Se inicializa el IEnumerable de los tokens llamando el método correspondiente del lex con el texto
+    //Se inicializan también las clases TokenStream, Parser para poder utilizar sus métodos y propiedades, la lista 
+    //de errores para ir guardándolos y el nodo de programa elemental que es el que evaluará todas las estructuras que se creen
+    Console.WriteLine("Iniciando el análisis");
+    
+    IEnumerable<Token> tokens = lex.GetTokens(text, new List<CompilingError>());
+    
+    TokenStream stream = new(tokens);
+    Parser parser = new(stream);
+    List<CompilingError> errors = new();
+    
+    MainProgram program = parser.Parse(errors);
+    
+    
+    //Si han ocurrido errores durante el proceso de análisis sintáctico se imprimen en consola, si no se pasa a realizar 
+    //el análisis semántico, se repite el proceso de que si han habido errores se imprimen y si no se evalúa y devuelven 
+    //los valores correspondientes.
+    
     if (errors.Count > 0)
     {
         foreach (var error in errors)
@@ -44,8 +39,18 @@ else
     }
     else
     {
-        Console.WriteLine("Iniciando Evaluación");
-        program.Evaluate(context, scope);
-        //Console.WriteLine(program);
+        program.CheckSemantic(context, scope, errors);
+    
+        if (errors.Count > 0)
+        {
+            foreach (var error in errors)
+                Console.WriteLine("{0}, {1}, {2}", error.Location.Column, error.Code, error.Argument);
+        }
+        else
+        {
+            Console.WriteLine("Iniciando Evaluación");
+            program.Evaluate(context, scope);
+            //Console.WriteLine(program);
+        }
     }
 }
